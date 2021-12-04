ce512b9d8a1fdba2e0ecd3f5b904b5ee393382c2
Celinesco <celina.alsina@gmail.com>
2021-12-04 11:17:42 -0300

    modifique fomra de seleccionar por id

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const botonAgregarUsuario = document.getElementById("boton-submit");
-const formularioNuevoUsuario = document.getElementById("formulario-nuevo-usuario");
 const inputNombreUsuario = document.getElementById("user-name");
 const inputEmailUsuario = document.getElementById("user-mail");
 const inputDireccionUsuario = document.getElementById("user-address");
 const inputTelefonoUsuario = document.getElementById("user-number");
 const tablaDeUsuarios = document.getElementById("tabla-usuarios");
 const textoAdvertencia = document.querySelector(".texto-advertencia");
-const camposRequeridos = document.querySelectorAll(".requerido")
+const camposRequeridos = document.querySelectorAll(".requerido");
+const body = document.querySelector("body");
+const contenedorFormulario = document.querySelector(".contenedor-formulario")
+
+//variables Auxiliares 
 let camposEnBlanco = false;
+let valoresPreviosAEditar = []
 
 const actualizarListaBotonesEliminar = () => {
   let listaBotonesEliminar = document.querySelectorAll(".botones-eliminar");
@@ -33,8 +37,8 @@ const tablaEnHTML = (data) => {
         <td>${element.email}</td>
         <td>${element.address}</td>
         <td>${element.phone}</td>
-        <td><button class="botones-eliminar acciones" id="eliminar${element.id}">Eliminar</button>
-          <button class="botones-editar acciones" id="editar${element.id}">Editar</button>
+        <td><button class="botones-eliminar acciones" data-id="${element.id}">Eliminar</button>
+          <button class="botones-editar acciones" data-id="${element.id}">Editar</button>
         </td>
       </tr>
       `
@@ -50,6 +54,7 @@ const tablaEnHTML = (data) => {
 
   tablaDeUsuarios.innerHTML = tabla;
   eliminarUsuario()
+  editarUsuario()
 }
 
 
@@ -62,7 +67,7 @@ const crearNuevoUsuarioAPI = (user) => {
       "Content-Type": "application/json"
     }
   }).then((res) => res.json())
-  .then ((data)=> {
+  .then (()=> {
     informacionApi()
   })
 }
@@ -95,15 +100,11 @@ botonAgregarUsuario.onclick = (e) => {
 
 const eliminarUsuarioEnLaApi = (id) => {
   fetch(`https://601da02bbe5f340017a19d60.mockapi.io/users/${id}`, {
-  method: "DELETE", 
-  headers: {
-    "Content-Type": "application/json"
-  }
-}).then((res) => res.json())
-.then((data) => {
-  informacionApi()
-})
-
+    method: "DELETE",
+  }).then((res) => res.json())
+    .then(() => {
+      informacionApi()
+    })
 }
 
 
@@ -111,13 +112,42 @@ const eliminarUsuario = () => {
   let listaBotonesEliminar = actualizarListaBotonesEliminar();
   listaBotonesEliminar.forEach((boton)=> {
     boton.onclick = () => {
-      let cantidadLetrasPalabraEliminar = 8;
-      let botonEliminarId = boton.id;
-      let idLimpio = botonEliminarId.slice(cantidadLetrasPalabraEliminar)
-      eliminarUsuarioEnLaApi(idLimpio)
+      let idElemento = boton.dataset.id
+      eliminarUsuarioEnLaApi(idElemento)
     }
   })
 }
+
+
+const obtenerUsuarioAEditar = (id) => {
+  fetch(`https://601da02bbe5f340017a19d60.mockapi.io/users/${id}`)
+  .then((res) => res.json())
+    .then((data) => {
+      let guardar = data
+      return guardar
+    })
+}
+
+
+const editarUsuario = () => {
+  let listaBotonesEditar = document.querySelectorAll(".botones-editar")
+  listaBotonesEditar.forEach((boton)=> {
+    boton.onclick = () => {
+      let idElemento = boton.dataset.id
+
+      tablaDeUsuarios.classList.add("oscurecer");
+      contenedorFormulario.classList.add("posicion");
+      let prueba = obtenerUsuarioAEditar(idElemento)
+      console.log(prueba)
+      
+
+    }
+  })
+}
+
+
+
+
 
 
 camposRequeridos.forEach ((input)=> {
@@ -129,4 +159,12 @@ camposRequeridos.forEach ((input)=> {
 
 
 
+
+
+
 informacionApi()
+
+//cambiar id por data-id:${element.id}
+//dataset.id
+
+//
